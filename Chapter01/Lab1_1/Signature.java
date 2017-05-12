fafe0f5a9c55e0572bf9702670c9c67859b6cbc2
Rui Amaro <rui.amaro@outlook.com>
2017-05-12 12:21:26 +0100

    Capitulo 1 concluido

--- Chapter01/Lab1_1/Signature.java
+++ Chapter01/Lab1_1/Signature.java
@@ -1,4 +1,4 @@
-public class Lab1_1 {
+public class Signature {
   public static void main(String [] args) {
     System.out.println("Rui Amaro");
     System.out.println("rui.amaro@gmail.com");
---
+++ Chapter01/Lab1_2/Book.java
@@ -0,0 +1,6 @@
+public class Book {
+  public static void main(String [] args) {
+    System.out.println("Title: " + args[0]);
+		System.out.println("Author: " + args[1]);
+  }
+}
